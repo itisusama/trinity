376678947dbca77cb1f59374e81c9cecd4185821
Usama Hassan <ahmarkhan1233456@gmail.com>
2025-08-30 14:24:11 +0500

    api deployed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -50,7 +50,7 @@ const ContactForm = () => {
 
     try {
       setStatus("Sending...");
-      const response = await fetch("http://localhost:5005/send-email", {
+      const response = await fetch("https://trinity-send.vercel.app/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
