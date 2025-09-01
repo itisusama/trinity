1ae2be6a8fb1e34e75fe548696cd6b7c88dc608e
Usama Hassan <ahmarkhan1233456@gmail.com>
2025-09-01 11:10:18 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,10 @@ const App = () => {
         </Routes>
         <Footer/>
       </Router>
-
+{/*  */}
       {/* WhatsApp Floating Button */}
       <a
-        href="https://wa.me/+92321647336"
+        href="https://wa.me/923216473336"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
